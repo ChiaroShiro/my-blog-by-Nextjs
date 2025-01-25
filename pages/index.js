c4768547646394945f73531cf2bd8b9c13c58252
Chiaro <ChiaroShiro@outlook.com>
2025-01-25 19:52:41 +0800

    实现文章窗格背景虚化

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
+import Layout from '../components/layout'
 import { getSortedPostsData } from '../lib/posts'
 import Link from 'next/link'
 import Date from '../components/date'
