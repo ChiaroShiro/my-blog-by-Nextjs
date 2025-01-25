f7b312d9d6accae979adeb4197f23e887ca30ac7
Chiaro <ChiaroShiro@outlook.com>
2025-01-25 19:40:46 +0800

    首页文章列表增加封面

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,27 @@
-import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
 import Link from 'next/link'
 import Date from '../components/date'
+import styles from '../styles/Home.module.css'
 
 export default function Home({ allPostsData }) {
   return (
     <Layout home>
-      <Head>…</Head>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>
-                {title}
-              </Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
+      <section className={styles.blogGrid}>
+        {allPostsData.map(({ id, date, title, picadd, shortContent }) => (
+          <Link href={`/posts/${id}`} key={id}>
+            <div className={styles.blogCard} style={{backgroundImage: `url(${picadd})`}}>
+              <div className={styles.blogContent}>
+                <h2 className={styles.blogTitle}>{title}</h2>
+                <p className={styles.blogExcerpt}>{shortContent}...</p>
+                <small className={styles.blogDate}>
+                  <Date dateString={date} />
+                </small>
+              </div>
+            </div>
+          </Link>
+        ))}
       </section>
     </Layout>
   )
