2d159be65c00b5ee2052d522419d534bc89fd93e
Chiaro <ChiaroShiro@outlook.com>
2025-01-27 20:53:36 +0800

    新增无图片博文、新增首页博文标签显示

--- components/blogCard.js
+++ components/blogCard.js
@@ -4,8 +4,9 @@ import styles from '../styles/blogCard.module.css'
 import { useEffect, useState } from 'react'
 import Image from 'next/image'
 import { processMarkdownContent } from '../lib/makeMarkdown'
+import Tags from './tags'
 
-export default function BlogCard({ id, date, title, picadd, shortContent }) {
+export default function BlogCard({ id, date, title, picadd, shortContent, tags }) {
   const [processedContent, setProcessedContent] = useState(shortContent)
   
   useEffect(() => {
@@ -21,15 +22,20 @@ export default function BlogCard({ id, date, title, picadd, shortContent }) {
 
   return (
     <Link href={`/posts/${id}`}>
-      <div className={styles.blogCard}>
-        <Image 
-          src={picadd} 
-          alt={title} 
-          fill={true}
-          className={styles.blogImage}
-        />
+      <div className={picadd ? styles.blogCard : styles.blogCardNoImage}>
+        {picadd && (
+          <Image 
+            src={picadd} 
+            alt={title} 
+            fill={true}
+            className={styles.blogImage}
+          />
+        )}
         <div className={styles.blogContent}>
-          <h2 className={styles.blogTitle}>{title}</h2>
+          <h2 className={styles.blogTitle}>
+            <span>{title}</span>
+            <Tags tags={tags} />
+          </h2>
           <p className={styles.blogExcerpt} 
              dangerouslySetInnerHTML={{__html: processedContent}} />
           <small className={styles.blogDate}>
--- pages/index.js
+++ pages/index.js
@@ -7,13 +7,14 @@ export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <section className={styles.blogGrid}>
-        {allPostsData.map(({ id, date, title, picadd, shortContent }) => (
+        {allPostsData.map(({ id, date, title, picadd, shortContent, tags }) => (
           <BlogCard
             key={id}
             id={id}
             date={date}
             title={title}
             picadd={picadd}
+            tags={tags}
             shortContent={shortContent}
           />
         ))}
