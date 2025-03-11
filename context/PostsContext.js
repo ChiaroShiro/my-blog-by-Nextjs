2a04e98d572f7a93b08c588135e03e6360c111c4
Chiaro <ChiaroShiro@outlook.com>
2025-03-11 11:14:03 +0800

    增加_app.js里默认值

--- context/PostsContext.js
+++ context/PostsContext.js
@@ -3,7 +3,7 @@ import { createContext } from 'react'
 // 建造一个叫 PostsContext 的广播塔
 export const PostsContext = createContext()
 
-export function PostsProvider({ children, value }) {
+export function PostsProvider({ children, value = { allPostsData: [] } }) {
   return (
     <PostsContext.Provider value={value}>
       {children}
